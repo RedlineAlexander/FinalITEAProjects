c508703b615eda5ad61fcb594115eaf6c3b4d4ec
RedlineAlexander <redlinerailmetro@gmail.com>
2020-08-21 11:35:48 +0300

    SQLREPOSITORIESSTUDENT1.4

--- FinalITEAProjects/Models/Repositories/SqlStudentRepository.cs
+++ FinalITEAProjects/Models/Repositories/SqlStudentRepository.cs
@@ -17,27 +17,53 @@ namespace FinalITEAProjects.Models.Repositories
 
         public void CreateStudent(Student student)
         {
+            _context.Students.Add(student);
+            _context.SaveChanges();
             //throw new NotImplementedException();
         }
 
         public void DeleteStudent(int studentId)
         {
+            var DeleteStudent = _context.Students.SingleOrDefault(x => x.ID == studentId);
+            try
+            {
+                _context.Students.Remove(DeleteStudent);
+            }
+            catch
+            {
+                throw new ArgumentNullException($"There is no student with Id = {studentId}");
+            }
+            finally
+            {
+                _context.SaveChanges();
+            }
             //throw new NotImplementedException();
         }
 
         public IEnumerable<Student> GetAllStudents()
         {
+            return _context.Students;
            // throw new NotImplementedException();
         }
 
         public Student GetStudent(int studentId)
         {
+            return _context.Students.SingleOrDefault(x => x.ID == studentId);
            // throw new NotImplementedException();
         }
 
         public void ModifyStudent(int studentId, Student student)
         {
+            var studentToUpdate = _context.Students.SingleOrDefault(x => x.ID == studentId);
+            if (studentToUpdate == null)
+                return;
+
+            studentToUpdate.FirstMidName = student.FirstMidName;
+            studentToUpdate.LastName = student.LastName;
+            studentToUpdate.EnrollmentDate = student.EnrollmentDate;
+            studentToUpdate.Enrollments = student.Enrollments;
             //throw new NotImplementedException();
+            _context.SaveChanges();
         }
     }
 }
