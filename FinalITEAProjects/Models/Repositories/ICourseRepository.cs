19381a79c22302dab59e4b9d59f563571e0111ec
RedlineAlexander <redlinerailmetro@gmail.com>
2020-08-21 10:05:58 +0300

    MyREPOSITORIES 1.2

---
+++ FinalITEAProjects/Models/Repositories/ICourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalITEAProjects.Models.Repositories
+{
+   public interface ICourseRepository
+    {
+        public IEnumerable<Course> GetAllCourses();
+        public Course GetCourse(int courseId);
+        public void CreateCourse(Course course);
+        public void ModifyCourse(int courseId, Course course);
+        public void DeleteCourse(int courseId); 
+    }
+}
---
+++ FinalITEAProjects/Models/Repositories/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalITEAProjects.Models.Repositories
+{
+    public interface IStudentRepository
+    {
+        public IEnumerable<Student> GetAllStudents();
+        public Student GetStudent(int studentId);
+        public void CreateStudent(Student student);
+        public void ModifyStudent(int studentId, Student student);
+
+        public void DeleteStudent(int studentId);
+    }
+}
